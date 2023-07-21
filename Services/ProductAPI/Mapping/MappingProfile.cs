964b429cc8767dc351d98d8a8e452b38ea5ad77d
vasylhulpak <vh@interlogic.com.ua>
2023-07-21 10:58:06 +0300

    Added mapping to products

---
+++ Services/ProductAPI/Mapping/MappingProfile.cs
@@ -0,0 +1,11 @@
+﻿using AutoMapper;
+
+namespace ProductAPI.Mapping;
+
+public class MappingProfile : Profile
+{
+	public MappingProfile()
+	{
+
+	}
+}
--- Services/ProductAPI/Program.cs
+++ Services/ProductAPI/Program.cs
@@ -1,3 +1,4 @@
+using ProductAPI.Mapping;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +7,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(typeof(MappingProfile));
 
 var app = builder.Build();
 
