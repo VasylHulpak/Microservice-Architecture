a75a05597327a54912ce128b4e5949b425ef43bc
vasylhulpak <vh@interlogic.com.ua>
2023-07-22 10:46:15 +0300

    Added default layout

--- Gateway.WebAPI/src/main.ts
+++ Gateway.WebAPI/src/main.ts
@@ -1,9 +1,10 @@
 import { createApp } from 'vue'
 import './style.css'
+import router from './router'
 import App from './App.vue'
 import 'virtual:windi.css'
 import 'virtual:windi-devtools'
 import 'vant/lib/index.css'
 
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
